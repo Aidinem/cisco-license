d2dc5a5c9e6a702f60ffab4b8029d79e34b5d1d9
aidinem <aidintaghavian20@gmail.com>
2021-08-24 16:01:08 +0430

    Adding Header section to our project

--- src/components/collection-preview/collection-priview.component.jsx
+++ src/components/collection-preview/collection-priview.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./collection-priview.style.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 
 const CollectionPreview = ({title, items}) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.filter((items, idx) => idx < 4).map((item) => (
-                <div key={item.id}>{item.name}</div>
+            {items.filter((items, idx) => idx < 4).map(({id, ...otherItemsProps}) => (
+                <CollectionItem key={id} {...otherItemsProps}/>
             ))}
         </div>
 
